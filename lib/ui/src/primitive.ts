859819ad96ac55e3c2acc532518a312996ee7836
David Peek <dpeek@users.noreply.github.com>
2026-05-22 17:40:13 +1000

    Cleanup

--- lib/ui/src/primitive.ts
+++ lib/ui/src/primitive.ts
@@ -1,9 +1,8 @@
-import { clsx, type ClassValue } from "clsx";
 import { composeRenderProps } from "react-aria-components/composeRenderProps";
 import { type ClassNameValue, twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: ClassNameValue[]) {
+  return twMerge(...inputs);
 }
 
 /** @deprecated Use cx */
